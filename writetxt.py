34b63768ca5aa1087a85d80cc1ccc8d9d02b0356
mathe <kk>
2022-05-11 14:16:18 -0300

    fiap

--- writetxt.py
+++ writetxt.py
@@ -1,16 +1,25 @@
+import json
+
 import streamlit as st
 import pandas
-import pip
-pip.main(["install", "openpyxl"])
+import requests as req
+# import pip
+# pip.main(["install", "openpyxl"])
 
 def main():
 
+
+    dict = req.get("https://sinapsebiotecnologia.github.io/teste.json")
+    dict = dict.json()
+
     excel_data_df = pandas.read_excel('teste.xlsx')
     json_str = excel_data_df.to_json()
-    if json_str != "None":
-        with open("teste.json", "w") as arquivo:
-            arquivo.write(json_str)
 
+    print(dict)
+    print(json_str)
+    # if json_str != "None":
+    #     with open("teste.json", "w") as arquivo:
+    #         json.dump(json_str,arquivo)
 
 if __name__ == '__main__':
     main()
